20d66b1c18d482d3d0a29f5bf0648641edb9e22e
Dipanshu Sharma <dipanshusharma266105@gmail.com>
2026-02-14 19:39:00 +0530

    clean

--- backend-node/src/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (id, email) => {
-    return jwt.sign({ id, sub: email }, process.env.SECRET_KEY, {
-        expiresIn: '24h' // Increased for better DX
-    });
-};
-
-module.exports = generateToken;
